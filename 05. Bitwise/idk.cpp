a22320d6b35a3f25e066a43f3eacc4da343e97e2
Vaibhav Chauhan <lunaticrealm07@gmail.com>
2024-02-15 01:42:22 +0530

    Updated

---
+++ 05. Bitwise/idk.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a = 4;
+    // int b = 3;
+    // int c = 2;
+    // bool d = 0;
+    // // cout<<(a&b);
+    // // cout<<(a|b);
+    // cout << (~c);
+    // cout << (~d);
+    int a = 5;
+    int b = 5;
+    cout << (a ^ b);
+    // XOR
+
+    return 0;
+}
