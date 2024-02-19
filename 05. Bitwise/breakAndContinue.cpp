c301d8195849d925da7f751d5713022a5ebbb7c8
Vaibhav Chauhan <lunaticrealm07@gmail.com>
2024-02-19 23:39:59 +0530

    Updated

---
+++ 05. Bitwise/breakAndContinue.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+
+    // for (int i = 0; i < 5; i++)
+    // {
+    //     cout << i << endl;
+
+    //     if (i == 3)
+    //     {
+    //         break;
+    //     }
+    // }
+
+    for (int i = 0; i < 5; i++)
+    {
+        cout << i << endl;
+
+        if (i == 3)
+            continue;
+    }
+    return 0; 
+}
---
+++ 05. Bitwise/prePostIncrement.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // int a = 5;
+    // cout << "Pehle incremented then used" << ++a << endl; // gives 6. This is preincrement
+
+    // cout << "Pehle used" << a++ << endl; // gives 5
+    // cout << "Then incremented" << a;
+
+    // loops
+
+    // for (int i = 0; i < 4; i++)
+    // {
+    //     cout << i;
+    // }
+    //   for (int i = 0; i < 4; ++i)
+    //     {
+    //         cout << i;
+    //     }
+
+   
+    return 0;
+}
